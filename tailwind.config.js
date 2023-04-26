5cb0e6c87712645c8f5decef3c95855b088ea577
Killian Kavanagh <Killian.Kavanagh@maintel.co.uk>
2023-04-26 09:26:29 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '500px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    extend: {
+      fontfamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+    },
+    letterSpacing: {
+      widest: '0.1em',
+    },
+  }
+  },
+  plugins: [],
+}
+
+
